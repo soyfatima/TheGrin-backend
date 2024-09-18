6e5a0defcb6eb45818e97a28f5f2db49d7c3cc60
soyfatima <imfatma02@gmail.com>
2024-09-18 10:51:30 +0000

    fix main file

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
 import * as express from 'express';
-import { config } from 'dotenv';
 import { join } from 'path';
-import * as cors from 'cors';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from '@nestjs/common';
-
-config();
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const server = express();
 
-  server.use(
-    cors({
-      origin: ['http://localhost:4200', 'http://localhost:3000'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
-    }),
-  );
+  const server = express();
+  dotenv.config();
 
   // Set the limit option for body-parser middlewares
   const bodyParserOptions = {
-    limit: '50mb', // Adjust the value as needed
+    limit: '150mb',
   };
 
   server.use(express.json(bodyParserOptions));
@@ -32,20 +21,44 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
-    new ExpressAdapter(server),
+    new ExpressAdapter(server)
   );
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
-
-  // Set up logger configuration
-  
-  app.useLogger(new Logger());
   app.use('/blog-backend/ProfilPic', express.static('ProfilPic'));
   app.use('/blog-backend/userFile', express.static('userFile'));
   app.use('/blog-backend/adminFile', express.static('adminFile'));
   app.use('/blog-backend/productFile', express.static('productFile'));
 
-  await app.listen(3000);
+  app.useLogger(new Logger());
+
+  const allowedOrigins = [
+    // 'https://thltechnologies.com', 'https://thlweb-admin.vercel.app', 'https://thltechserveur.com'
+  ];
+
+  // Custom CORS middleware to handle multiple origins
+  app.use((req, res, next) => {
+    const origin = req.headers.origin as string;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
+  // Serve the Angular application's index.html file for all other routes
+  app.use('*', express.static(join(__dirname, '..', 'public')));
+  // Serve the Angular application's index.html file for all other routes
+  app.use('*', (req, res, next) => {
+    express.static(join(__dirname, '..', 'public'))(req, res, next);
+  });
+
+  await app.listen(4000, '0.0.0.0');
 }
 
 bootstrap();
--- src/multerOptions.ts
+++ src/multerOptions.ts
@@ -3,12 +3,11 @@ import { extname } from 'path';
 
 export const multerOptions = {
   storage: diskStorage({
-    destination: './ProfilPic', // Destination folder for storing uploaded files
+    destination: './ProfilPic', 
     filename: (req, file, callback) => {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
       const fileExtension = extname(file.originalname);
       const fileName = `${uniqueSuffix}${fileExtension}`;
-      // Save the file in the 'profilePictures' folder
       callback(null, fileName);
     },
   }),
