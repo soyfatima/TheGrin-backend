cf2f40ec42f9b86628be49143882c95e7d106237
soyfatima <imfatma02@gmail.com>
2024-11-22 12:38:37 +0100

    add env

--- src/jwtGuard/config.ts
+++ src/jwtGuard/config.ts
@@ -6,7 +6,7 @@ config();
 export const jwtConfig = {
   secret:
     process.env.JWT_SECRET ||
-    '40d6d29df5c74a8424a0e0a58b9e8ad5f9537c91d4182a8dedf6f2f33bcba7bf26e5c4251ad5095858868233dbc3416cc6713aaacb5f41389efb8e0c23c3b951',
+    '87b848ba45e17a8a9e8f9b9076e50a8c3c80d24ac24c0525679cb3ef0d896177679d66b29188e73c3064165003609f3b95146bc37aca5ecde607e297c7f53af6',
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || uuidv4(),
   expiresIn: '15m',
   refreshTokenExpiresIn: '1d',
